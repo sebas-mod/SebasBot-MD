4bbfcc96fed79f1458b5bfb5541d423fab0e148d
Ancelbot <135066230+JossXvB@users.noreply.github.com>
2024-02-12 16:39:23 -0500

    𝑫𝑰𝑵𝑶𝑩𝑶𝑻 🦖

---
+++ plugins/bsmixto.js
@@ -0,0 +1,34 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.bsmixto);
+};
+handler.command = /^(bsmixto|buscarvsmixto)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.bsmixto = `
+🫧 ⋆ ☇ 𝘽𝙐𝙎𝘾𝙊 𝙑𝙎 𝙈𝙄𝙓𝙏𝙊 ˏ`୭̥ 👺
+
+    ›› 𝗛𝗼𝗿𝗮 ¦ 𝗠𝗼𝗱↷ 🕰️
+  
+⌦ 𝐌𝐨𝐝𝐚𝐥𝐢𝐝𝐚𝐝: 
+☁❩ 16:00 🇨🇱🇦🇷 ⌇ 14:00 🇵🇪
+⌦ 𝐌𝐨𝐝𝐚𝐥𝐢𝐝𝐚𝐝: 
+☁❩ 18:00 🇨🇱🇦🇷 ⌇ 16:00 🇵🇪
+⌦ 𝐌𝐨𝐝𝐚𝐥𝐢𝐝𝐚𝐝: 
+☁❩ 20:00 🇨🇱🇦🇷 ⌇ 18:00 🇵🇪
+⌦ 𝐌𝐨𝐝𝐚𝐥𝐢𝐝𝐚𝐝: 
+☁❩ 22:00 🇨🇱🇦🇷 ⌇ 20:00 🇵🇪
+⌦ 𝐌𝐨𝐝𝐚𝐥𝐢𝐝𝐚𝐝: 
+☁❩ 00:00 🇨🇱🇦🇷 ⌇ 22:00 🇵🇪 
+
+⊹ ִֶָ  🌷Mis reglas, No modifico
+⊹ ִֶָ  🌷1 🖥
+⊹ ִֶָ  👺Hablar con IG si o si.
+⊹ ִֶָ  👺Con logo en mano
+
+⚜️Para coordinar vs:🎀
+https://wa.me/
+https://wa.me/
+`;
