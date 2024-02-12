1aa6878c3789f1b32917e88ddcf5bec4b2bf2dcf
Ancelbot <135066230+JossXvB@users.noreply.github.com>
2024-02-11 23:53:06 -0500

    𝕯𝖎𝖓𝖔𝕭𝖔𝖙 🦖

---
+++ plugins/cccompras.js
@@ -0,0 +1,54 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.cccompras);
+};
+handler.command = /^(cccompras|tarjeta)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.cccompras = `
+NOTA: Prueba en sitios como CANVAPRO, APPLE TV, DUOLINGO,VIKIRAKUTEN,MUBI,ETC.\nCon IP del país de la CC\n\nTAMBIEN ENSEÑO A SACAR TUS PROPIAS PLATAFORMAS, QUE ESPERAS? HABLAME: Wa.me/56986469687
+
+⚜️ 𝑺𝒕𝒂𝒕𝒖𝒔 -» 𝑨𝒑𝒑𝒓𝒐𝒗𝒆𝒅 ✅
+⚜️ 𝑹𝒆𝒔𝒑𝒐𝒏𝒔𝒆 -» 𝑨𝒑𝒑𝒓𝒐𝒗𝒆𝒅
+⚜️ CC -» 4154644408664226|09|2031|680
+⚜️ 𝑮𝑨𝑻𝑬 -» 𝑩𝒓𝒂𝒊𝒏𝒕𝒓𝒆𝒆 ♻️
+╚━━━━━━「 𝑫𝑬𝑻𝑨𝑰𝑳𝑺 」━━━━━━╝
+⚜️ 𝑩𝒂𝒏𝒌: KHALEEJI COMMERCIAL BANK B.S.C.
+⚜️ 𝑪𝒐𝒖𝒏𝒕𝒓𝒚: BAHRAIN 🇧🇭 - BHD
+⚜️ 𝑰𝒏𝒇𝒐: DEBIT - VISA - CLASSIC
+╚━━━━━━「 𝑰𝑵𝑭𝑶 」━━━━━━╝
+⚜️ 𝑷𝒓𝒐𝒙𝒚 -» 𝑷𝒓𝒐𝒙𝒚 𝑳𝒊𝒗𝒆✅
+
+⚜️ 𝑺𝒕𝒂𝒕𝒖𝒔 -» 𝑨𝒑𝒑𝒓𝒐𝒗𝒆𝒅 ✅
+⚜️ 𝑹𝒆𝒔𝒑𝒐𝒏𝒔𝒆 -» Approved 
+⚜️ 𝑪𝑽𝑪 -» U - ⚜️ 𝑨𝑽𝑺 -» U
+⚜️ CC -» 5194710001061703|10|2026|285
+⚜️ 𝑮𝑨𝑻𝑬 -» 𝑱ö𝒓𝒎𝒖𝒏𝒅𝒈𝒂𝒏𝒅𝒆𝒓 ♻️
+╚━━━━━━「 𝑫𝑬𝑻𝑨𝑰𝑳𝑺 」━━━━━━╝
+⚜️ 𝑩𝒂𝒏𝒌: ALLIANCE BANK MALAYSIA BERHAD
+⚜️ 𝑪𝒐𝒖𝒏𝒕𝒓𝒚: MALAYSIA 🇲🇾 - MYR
+⚜️ 𝑰𝒏𝒇𝒐: DEBIT - MASTERCARD - STANDARD
+╚━━━━━━「 𝑰𝑵𝑭𝑶 」━━━━━━╝
+
+⚜️ 𝑺𝒕𝒂𝒕𝒖𝒔 -» 𝑨𝒑𝒑𝒓𝒐𝒗𝒆𝒅 ✅
+⚜️ 𝑪𝑽𝑪 -» unavailable - ⚜️ 𝑨𝑽𝑺 -» unavailable
+⚜️ CC -» 5194710006131774|05|2029|769
+⚜️ 𝑮𝑨𝑻𝑬 -» 𝑫𝒆𝒊𝒎𝒐𝒔 ♻️
+╚━━━━━━「 𝑫𝑬𝑻𝑨𝑰𝑳𝑺 」━━━━━━╝
+⚜️ 𝑩𝒂𝒏𝒌: ALLIANCE BANK MALAYSIA BERHAD
+⚜️ 𝑪𝒐𝒖𝒏𝒕𝒓𝒚: MALAYSIA 🇲🇾 - MYR
+⚜️ 𝑰𝒏𝒇𝒐: DEBIT - MASTERCARD - STANDARD
+╚━━━━━━「 𝑰𝑵𝑭𝑶 」━━━━━━╝
+
+⚜️ 𝑺𝒕𝒂𝒕𝒖𝒔 -» 𝑨𝒑𝒑𝒓𝒐𝒗𝒆𝒅 ✅
+⚜️ 𝑹𝒆𝒔𝒑𝒐𝒏𝒔𝒆 -» 𝑨𝒑𝒑𝒓𝒐𝒗𝒆𝒅 
+⚜️ CC -» 5326780009727648|11|2027|188
+⚜️ 𝑮𝑨𝑻𝑬 -» 𝑩𝒓𝒂𝒊𝒏𝒕𝒓𝒆𝒆 ♻️
+╚━━━━━━「 𝑫𝑬𝑻𝑨𝑰𝑳𝑺 」━━━━━━╝
+⚜️ 𝑩𝒂𝒏𝒌: ALLIANCE BANK MALAYSIA BERHAD
+⚜️ 𝑪𝒐𝒖𝒏𝒕𝒓𝒚: MALAYSIA 🇲🇾 - MYR
+⚜️ 𝑰𝒏𝒇𝒐: DEBIT - MASTERCARD - STANDARD
+╚━━━━━━「 𝑰𝑵𝑭𝑶 」━━━━━━╝
+`;
