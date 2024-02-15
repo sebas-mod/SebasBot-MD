8a694c9d1adc0ba817a2511e14548853b1258681
Ancelbot <135066230+JossXvB@users.noreply.github.com>
2024-02-15 00:02:51 -0500

    Create recursosdzn.js

---
+++ plugins/recursosdzn.js
@@ -0,0 +1,42 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.recursosdzn);
+};
+handler.command = /^(recursosdzn|drivesdzn|dzn)$/i;
+export default handler;
+global.recursosdzn = `
+
+RECURSOS DZN
+https://drive.google.com/folderview?id=165XNPaoGZ71ViU4__AtHtrS2DUdOWc7n
+
+https://drive.google.com/folderview?id=1SyHLm3eFFxV0kSXNBfwhc8oFJfijbGM9
+
+https://drive.google.com/folderview?id=19hfTP-r0x1j8GEbB8jRVVuVYxvuM2mJR
+
+https://drive.google.com/drive/folders/1AHuoeHbh-9jA3qwC0yNNnXQcs9sQOa1P?usp=sharing
+
+https://drive.google.com/drive/mobile/folders/1EuqEVuzuPYAjA3r0p5h3AjhxjClmPYWx
+
+https://drive.google.com/folderview?id=1zeuBSzzWVbJ9JZ7WZUs1bwEc30277G2p
+
+https://drive.google.com/folderview?id=1MAn8popYgy65Lq-qpsTGJAMCf1OcNzTD
+
+https://drive.google.com/folderview?id=1QT_drbbniUDnG8HtqSbUS-tTu8H7esxr
+
+https://drive.google.com/drive/folders/1ZN0b04Ek6XeIYr3TOrSEd20YFZxpSS5A?usp=sharing
+
+https://drive.google.com/folderview?id=1kmcNyCDe4-ZIV6gp1iKRv-4JDUnUGQll
+
+https://www.mediafire.com/file/eb18wvfhzxi8hmx/PACK+@RALKZDESIGN.rar
+
+https://mega.nz/folder/PHRGEJRD#5IBYfEmSOs97cdUEh-AzEA
+
+https://mega.nz/folder/fKAh0SjK#sGYcMWkUQfudm8_DIF1f3g
+
+https://mega.nz/folder/bXxinBLT#OJYdlhQBjncyN82TTOW5ZA
+
+https://mega.nz/folder/DOgQFbhC#zQPYN3J9776vxuclCkNAwg
+
+https://drive.google.com/folderview?id=165XNPaoGZ71ViU4__AtHtrS2DUdOWc7n
+
+https://drive.google.com/drive/folders/1X0IX6tU57AqL8VRYVJWmZ1py4DYiJ1Zu 
+`;
