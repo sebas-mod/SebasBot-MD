9fe7cc2bf717870dcfcb3c175ee60b06c609daa6
Ancelbot <135066230+JossXvB@users.noreply.github.com>
2024-02-15 00:08:13 -0500

    Update DriveEdits.js

--- plugins/DriveEdits.js
+++ plugins/DriveEdits.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn}) => {
   m.reply(global.DriveEdits);
 };
-handler.command = /^(DriveEdits|drivesdzn|recursosdzn2)$/i;
+handler.command = /^(DriveEdits|drivesdzn|recursosdzn2|dzn2)$/i;
 export default handler;
 global.DriveEdits = `
 𝗟𝗜𝗡𝗞𝗦 𝗗𝗥𝗜𝗩𝗘 𝗘𝗗𝗜𝗧𝗦
