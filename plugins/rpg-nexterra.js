869bdff1e359372449ed7dabb37917fcad52c54c
Ancelbot <135066230+JossXvB@users.noreply.github.com>
2024-02-16 19:30:06 -0500

    Update rpg-nexterra.js

--- plugins/rpg-nexterra.js
+++ plugins/rpg-nexterra.js
@@ -13,7 +13,7 @@ let expp = Math.floor(Math.random() * 5000)
   global.db.data.users[m.sender].joincount += tok
   global.db.data.users[m.sender].exp += expp
   
-let texto = `*MAPA NEXTERRA FREE FIRE 2023 🗺️*`
+let texto = `*MAPA NEXTERRA FREE FIRE  🗺️*`
 
 const fkontak = {
 	"key": {
