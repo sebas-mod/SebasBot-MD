a9ae3d2f40a2c36800d0a88f2049a64683acfbde
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-03-03 21:33:29 -0400

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,9 +8,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `*@LUDMI_SHOP_:* ${pesan}`;
   let teks = `*😎𝐅𝐀𝐂𝐇𝐀𝐒 𝐀𝐂𝐓𝐈𝐕𝐄𝐍𝐒𝐄 𝐎 𝐂𝐔𝐏𝐎*\n\n ${oi}\n\n- ̗̀  😝 𝐅𝐮𝐭𝐮𝐫𝐨𝐬 𝐜𝐮𝐩𝐨𝐬 !\n`;
   for (const mem of participants) {
-    teks += `💋➤ @${mem.id.split('@')[0]}\n`;
+    teks += `│🐣 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝙇𝙪𝙙𝙢𝙞𝙗𝙤𝙩 💝\n\n*💝*`;
+  teks += `*└* 𝑺𝒆𝒃𝒂𝒔𝑩𝒐𝒕 🐣\n\n*🐣*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
