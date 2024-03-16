091d802703c54048a233eec0d4323ff15ac237e5
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-03-15 21:31:02 -0400

    Actualizar noetiquetar.js

--- plugins/noetiquetar.js
+++ plugins/noetiquetar.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command, paypal }) => {
-conn.sendMessage(m.chat, {text:'*[ ⚠️ ] no etiquetes a unos de mis creadores si tienes alguna consulta o duda hablale a su privado solo por tema de 𝕯𝖎𝖓𝖔𝕭𝖔𝖙 🦖*'},  { quoted: m })
+conn.sendMessage(m.chat, {text:'*[ ⚠️ ] no etiquetes a unos de mis creadores si tienes alguna consulta o duda hablale a su privado solo por tema de 𝐒𝐞𝐛𝐚𝐬 𝐁𝐨𝐭 🐣*'},  { quoted: m })
 }
 handler.help = ['pagina']
 handler.tags = ['info']
